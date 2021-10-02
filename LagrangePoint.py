2889dfdc49e46ad4c4a931585cff309ab19d2ede
Lin Yen-Hsing <julius52700@gapp.nthu.edu.tw>
2021-10-02 17:02:52 +0800

    init

--- LagrangePoint.py
+++ LagrangePoint.py
@@ -2,15 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 import matplotlib.animation as animation
+from scipy.integrate import solve_ivp
 
 # Some Constants
-G = 6.67e-11
-day2s = 86400 # s
-yr2s = 86400*365.2425 # s
-dt = day2s # s
-tspan = 5*yr2s # s
-omega = np.array([0, 0, 2*np.pi/yr2s]) # 1/s
-rE = 149.6e9 # m
+N = int(1e3)
+tspan = np.linspace(0, 5*yr2s, N) # s
 
 # Initial Condition
 #Init = [rE*np.cos(np.pi/3), rE*np.sin(np.pi/3), 1e3, 1e3]
@@ -21,95 +17,15 @@ plt.style.use('dark_background')
 fig, ax = plt.subplots(figsize=(6, 6))
 ax.set_facecolor('#303030')
 
-class BigBody:
-
-    def __init__(self, M, x, y, z):
-        self.M = M
-        self.x = x
-        self.y = y
-        self.z = z
-        return
 
 Sun   = BigBody(2e30, 0, 0, 0)
 Earth = BigBody(6e24, rE, 0, 0)
 
 SourceList = [Sun, Earth]
         
-class probe:
-
-    def __init__(self, x, y, z, vx, vy, vz):
-        self.x  = x
-        self.y  = y
-        self.z = z
-        self.vx = vx
-        self.vy = vy
-        self.vz = vz
-        self.acor = np.array([0,0,0])
-        self.acen = np.array([0,0,0])
-        self.ag =   np.array([0,0,0])
-        return
-    
-    def get_a(self):
-        
-        # Creating an array to store force
-        a = np.zeros(3)
-        ag = np.zeros(3)
-        # Gravity
-        for body in SourceList:
-            vec_r = np.array([(self.x - body.x), (self.y - body.y), (self.z - body.z)])
-            r = np.linalg.norm(vec_r)
-            ag += -G*body.M/r**3*vec_r
-        self.ag = ag
-        a += ag
-
-        # Centrifugal
-        acen = -np.cross(omega, np.cross(omega, np.array([self.x, self.y, self.z])))
-        a += acen
-        self.acen = acen
-        
-        # Coriolis
-        acor = -2*np.cross(omega, np.array([self.vx, self.vy, self.vz]))
-        a += acor
-        self.acor = acor
-        
-        # Euler force is 0 since dw/dt=0 (assuming circular motion of Earth)
-
-        return a
-
-    def get_P(self): # The position of the probe in phase space (since there are so many "p")
-        return np.array([self.x, self.y, self.z, self.vx, self.vy, self.vz])
-
-    def add_P(self, k):
-        self.x  += k[0]
-        self.y  += k[1]  
-        self.z  += k[2]  
-        self.vx += k[3]
-        self.vy += k[4]
-        self.vz += k[5]
-
-    def assign_P(self, P):
-        self.x  = P[0]
-        self.y  = P[1]  
-        self.z  = P[2]  
-        self.vx = P[3]
-        self.vy = P[4]
-        self.vz = P[5]
-
-    def get_dPdt(self):
-        a = self.get_a()
-        return np.array([self.vx, self.vy, self.vz, a[0], a[1], a[2]])
-
-    def update(self, dt):
-        p0 = self.get_P()
-        k1 = self.get_dPdt()*dt
-        self.add_P(k1)
-        k2 = self.get_dPdt()*dt
-        self.assign_P(p0)
-        self.add_P((k1+k2)/2)
-        return
 
 #Probe = probe(-1.4e11, 0, 0, -5e3)
-Probe = probe(Init[0],Init[1],Init[2],Init[3], Init[4], Init[5])
+Probe = probe(Init[0],Init[1],Init[2],Init[3], Init[4], Init[5], SourceList, tspan)
 
 # To record the position of the probe
 X = np.zeros(int(tspan/dt))
---
+++ constants.py
@@ -0,0 +1,6 @@
+# This file stores the constants that we will need
+G = 6.67e-11
+day2s = 86400 # s
+yr2s = 86400*365.2425 # s
+omega = np.array([0, 0, 2*np.pi/yr2s]) # 1/s
+rE = 149.6e9 # m
